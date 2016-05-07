cfb07ebe5b451d5d8a4e566b83c6c00f79be89b0
Ysee MONNIER <yseemonnier@gmail.com>
2016-05-07 18:55:20 +0200

    add nodes

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -10,11 +10,11 @@ var graph  = (function() {
 })();
 
 graph.modules.config = (function() {
+    var index = 0;
     return {
         init: function() {
             //                    modal.css("display", "none");//.style.display = "none";
             var modal = $('#modal-config');
-
             $('#add-node').click(function(e) {
                 e.preventDefault();
                 console.log('Click add node');
@@ -22,16 +22,29 @@ graph.modules.config = (function() {
                     graph.modules.config.addNode($('#input-node'));
                 }
             });
-
             $('#add-link').click(function(e) {
                 console.log('Click add link');
+                var s = $('#source').find(":selected").text();
+                var t = $('#target').find(":selected").text();
             });
         },
         addNode: function(inputName) {
+            var name = inputName.val()
+            var t = $('#nodes span').text() + (graph.nodes.length != 0 ? ', ' : '') + name;
+            $('#nodes span').text(t);
+
+            $("select[name='target-picker']").append($('<option></option>').val(index).html(name));
+            $("select[name='source-picker']").append($('<option></option>').val(index).html(name));
+
             graph.nodes.push({
                 'name': inputName.val()
-            })
+            });
             inputName.val('');
+            index++;
+            console.log(t);
+        },
+        addLink: function() {
+
         }
     }
 })();
